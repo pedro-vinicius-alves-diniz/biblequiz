b2e2d5a7169d4542c1d4242cf93b5eecb7018b0f
Pedro Vinicius Alves Diniz <pedrinhovini318@icloud.com>
2024-11-04 17:28:09 +0100

    implementado o game over

--- src/js/script.js
+++ src/js/script.js
@@ -3,19 +3,27 @@ import { perguntas } from "./database.js";
 
 
 //DECLARAÇÃO DE VARIAVEIS
+const containerQuiz = document.getElementById('container-quiz');
 const question = document.getElementById('pergunta');
 const option1 = document.getElementById('option1');
 const option2 = document.getElementById('option2');
 const option3 = document.getElementById('option3');
 const option4 = document.getElementById('option4');
 const anwser = document.getElementById('correct')
+const containerPosGame = document.getElementById("container-posgame");
+const btnAgain = document.getElementById("play-again");
+const btnClose = document.getElementById("close")
+const spanScore = document.getElementById("score")
 var score = 0;
 var currentQuestion = 1
 
 
 //FUNÇÕES
 function updateScreen(event) {
-    console.log('Score: ',score)
+    // containerQuiz.style.display = "flex";
+    // containerPosGame.style.display = "none";
+
+    console.log('Score: ', score)
     console.log('Rodada: ', currentQuestion)
 
     var indexQuiz = Math.floor(Math.random() * 46)
@@ -31,17 +39,17 @@ function updateScreen(event) {
 
 // ALGORITMO DE PONTUAÇÃO
 const systemScore = (event) => {
-    if(event.target.textContent === anwser.textContent){
-        score +=1
+    if (event.target.textContent === anwser.textContent) {
+        score += 1
     }
 
-    if(currentQuestion === 10){
-        alert("Acabou o jogo");
-        alert(score)
-        window.location.assign(window.location.href);
+    if (currentQuestion === 10) {
+        containerQuiz.style.display = "none";
+        containerPosGame.style.display = "flex";
+        spanScore.innerHTML = score
 
-    }else{
-        currentQuestion +=1
+    } else {
+        currentQuestion += 1
         updateScreen();
     }
 }
@@ -56,4 +64,17 @@ option1.addEventListener('click', systemScore);
 option2.addEventListener('click', systemScore);
 option3.addEventListener('click', systemScore);
 option4.addEventListener('click', systemScore);
+
+btnAgain.addEventListener('click', () => {
+    containerQuiz.style.display = "flex";
+    containerPosGame.style.display = "none";
+
+    score = 0;
+    currentQuestion = 1;
+    console.clear();
+    updateScreen();
+});
+btnClose.addEventListener('click', () => {
+    window.close()
+});
 
