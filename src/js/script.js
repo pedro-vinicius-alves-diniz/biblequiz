5c09c2dac16410e207606193737424f2d8c648c6
Pedro Vinicius Alves Diniz <pedrinhovini318@icloud.com>
2024-11-04 19:19:21 +0100

    implementado algoritmo de temporizador.

--- src/js/script.js
+++ src/js/script.js
@@ -3,13 +3,16 @@ import { perguntas } from "./database.js";
 
 
 //DECLARAÇÃO DE VARIAVEIS
+const containerStart = document.getElementById('container-start')
+const btnStart = document.getElementById("btn-start")
 const containerQuiz = document.getElementById('container-quiz');
+const clock = document.getElementById("clock")
 const question = document.getElementById('pergunta');
 const option1 = document.getElementById('option1');
 const option2 = document.getElementById('option2');
 const option3 = document.getElementById('option3');
 const option4 = document.getElementById('option4');
-const anwser = document.getElementById('correct')
+const anwser = document.getElementById('correct');
 const containerPosGame = document.getElementById("container-posgame");
 const btnAgain = document.getElementById("play-again");
 const btnClose = document.getElementById("close")
@@ -17,11 +20,28 @@ const spanScore = document.getElementById("score")
 var score = 0;
 var currentQuestion = 1
 
+let timeoutId;
+let intervalId
+
+const audioClock = document.getElementById('audio-clock');
+audioClock.loop = true
 
 //FUNÇÕES
 function updateScreen(event) {
-    // containerQuiz.style.display = "flex";
-    // containerPosGame.style.display = "none";
+    clock.innerText = 7;
+    
+
+    intervalId = setInterval (() =>{ 
+        clock.innerText -= 1
+        
+    }, 1000)
+
+    timeoutId = setTimeout(() => {
+        clearInterval(intervalId);
+        sytemNextQuestion();
+    }, 7000)
+
+    
 
     console.log('Score: ', score)
     console.log('Rodada: ', currentQuestion)
@@ -43,22 +63,35 @@ const systemScore = (event) => {
         score += 1
     }
 
+    sytemNextQuestion();
+}
+
+const sytemNextQuestion = () => {
     if (currentQuestion === 10) {
         containerQuiz.style.display = "none";
         containerPosGame.style.display = "flex";
         spanScore.innerHTML = score
+        clearInterval(intervalId);
+        clock.innerText = 0
+        audioClock.pause();
 
     } else {
-        currentQuestion += 1
+        currentQuestion += 1;
+        clearTimeout(timeoutId);
+        clearInterval(intervalId);
         updateScreen();
     }
 }
 
-// ALGORITMO DE TEMPO
-
 
 // EVENTOS
-window.addEventListener('load', updateScreen);
+// window.addEventListener('load', updateScreen);
+btnStart.addEventListener("click", () => {
+    containerStart.style.display = 'none';
+    containerQuiz.style.display = "flex";
+    updateScreen();
+    audioClock.play();
+})
 
 option1.addEventListener('click', systemScore);
 option2.addEventListener('click', systemScore);
@@ -77,4 +110,5 @@ btnAgain.addEventListener('click', () => {
 btnClose.addEventListener('click', () => {
     window.close()
 });
+
 
